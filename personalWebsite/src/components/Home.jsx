9f9f629f01ca6bba818304fd7d0835e365f138f4
EricLou <loueric11@gmail.com>
2024-08-03 22:06:59 -0400

    Update Home.jsx
    Updated About me page content

--- personalWebsite/src/components/Home.jsx
+++ personalWebsite/src/components/Home.jsx
@@ -7,10 +7,12 @@ function Home() {
     <section className="home-section">
       <div className="home-details">
         <h2>Let's work together
-        <span className='dot'>.</span>
+        <span className='dot'>!</span>
         </h2>
         <p>
-          I'm an <strong>independent creative developer</strong> from Abergavenny, South Wales. From interaction design to scalable design systems, single-page apps to something more experimental with WebGL. I help awesome people to build ambitious yet accessible web projects - <strong>the wilder, the better.</strong>
+          I'm a <strong>creative, analytical, communication-oriented</strong> 3rd year student at the University of Toronto studying computer engineering. I have worked on several web service applciations that achieve excellence in both usibility and accessbility. In my free time, I love to explore on new technologies and automation related projects! 
+          I am also someone who takes initiative and able to come up with 
+          <strong> unique and creative</strong> solutions to solve problems.
         </p>
         <button className="home-button">About my approach</button>
       </div>
