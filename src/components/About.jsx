1198d1975e41838311f3d36b7f8c134d29c79860
ericl0u <loueric11@gmail.com>
2024-09-04 20:11:49 -0400

    Reworked about screen
    Changed overall flex distribution, added few key animation to scroll word

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,16 +7,32 @@ import './styles/styles.css'
 function About() {
   return (
     <section className="about-section">
+
       <div className="about-content">
-        <img src={profilePic} alt="Profile" className="profile-pic" />
-        <h1>Hi, my name is   
+        
+        <h1 className='intro bitter-title'>Hi, 
+          <br />
+          my name is   
             <strong> Eric</strong>
             <span className='dot'>.</span>
         </h1>
 
-        <p className="scroll-text">SCROLL</p>
+        <h2 className='description bitter-title'> 
+          I am <strong>3rd year Computer Engineering student  </strong>from the University of Toronto.
+        </h2>
+        <p className="scroll-text bitter-title">
+          <span>S</span>
+          <span>C</span>
+          <span>R</span>
+          <span>O</span>
+          <span>L</span>
+          <span>L</span>
+        </p>
+        <div className="scroll-line"></div>
       </div>
-      <div className="scroll-line"></div>
+
+        <img src={profilePic} alt="Profile" className="profile-pic" />
+
     </section>
   );
 }
