cab8faff27d972bfecd7743b653353bb58307861
ericl0u <loueric11@gmail.com>
2024-09-05 21:22:21 -0400

    Added Animation for home section
    Added Animation for home section, improve styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Home.css';
 import './styles/styles.css'
 
 function Home() {
+  useEffect(() => {
+    const header = document.querySelector('.home-details h2');
+    const paragraph = document.querySelector('.home-details p')
+    const button = document.querySelector('.home-button')
+
+    const observer = new IntersectionObserver((entries) => {
+
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show'); 
+          setTimeout(() => {
+            paragraph.classList.add('show');
+            button.classList.add('show')
+          }, 1000);
+        }
+      });
+    });
+    observer.observe(header);
+  }, [])
+
+
+
+
   return (
     <section className="home-section">
       <div className="home-details bitter-home">
@@ -14,7 +37,7 @@ function Home() {
           I am also someone who takes initiative and able to come up with 
           <strong> unique and creative</strong> solutions to solve problems.
         </p>
-        <button className="home-button bitter-home">About my approach</button>
+        <button className="home-button bitter-home">More about me</button>
       </div>
     </section>
   );
