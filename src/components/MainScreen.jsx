ce0131cf2ee82b569d93d6246c2cf3af461d66e8
ericl0u <loueric11@gmail.com>
2024-09-14 21:57:24 -0400

    Update MainScreen.jsx
    Added comments

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -26,6 +26,7 @@ function mainScreen() {
       ></div>
       <Header />
       <ResponsiveAppBar></ResponsiveAppBar>
+      {/* needs adjustment for now */}
         <>
           <main>
             <About />
