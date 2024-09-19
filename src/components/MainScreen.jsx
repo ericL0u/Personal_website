a14010b772af2625f04dd2eef2288088b3b8bfdc
EricLou <loueric11@gmail.com>
2024-09-19 14:24:04 -0400

    Responsiveness improvements
    Added responsiveness changes to work, home sections.

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -30,8 +30,8 @@ function mainScreen() {
         <>
           <main>
             <About />
-            {/* <Home />
-            <Work /> */}
+            <Home />
+            <Work />
           </main>
           <Footer />
         </>
